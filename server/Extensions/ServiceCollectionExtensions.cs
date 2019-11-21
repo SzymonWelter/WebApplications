6b65eca9ed0515304ecd2d8ac837b51c2697e7f2
Szymon Welter <SZW@kmd.dk>
2019-11-21 11:15:55 +0100

    jwt authorization added

---
+++ server/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace server.Helpers {
+    public static class ServiceCollectionExtensions {
+        public static void AddJwtAuthorization (this IServiceCollection services, IConfiguration configuration) {
+
+            var secret = configuration["Authorization:Secret"];
+            var key = Encoding.ASCII.GetBytes (secret);
+
+            services.AddAuthentication (options => {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer (options => {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey (key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+        }
+    }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -1,57 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using server.Helpers;
 using server.Services.MapService;
 using server.Services.Repositories;
 
-namespace server
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace server {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddSingleton<IUsersRepository, UsersRepository>();
-            services.AddTransient<IMapService, MapService>();
-            services.AddCors(opt => opt.AddPolicy("Any", builder =>
-            {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddSingleton<IUsersRepository, UsersRepository> ();
+            services.AddTransient<IMapService, MapService> ();
+
+            services.AddJwtAuthorization (Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseRouting();
-            app.UseCors("Any");
+            app.UseRouting ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseCors (options => options
+                .AllowAnyOrigin ()
+                .AllowAnyMethod ()
+                .AllowAnyHeader ());
+
+            app.UseAuthentication ();
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
