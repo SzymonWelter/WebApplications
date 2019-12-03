b227dd5369ecc31b16b6756315fad3e4b1b78d71
Szymon Welter <SZW@kmd.dk>
2019-12-03 18:43:09 +0100

    view of home page imroved

--- client/src/js/home/Sections/FilesSection/FileTile.jsx
+++ client/src/js/home/Sections/FilesSection/FileTile.jsx
@@ -4,7 +4,7 @@ export function FileTile(props){
     return(
         <div className="file-tile__wrapper">
             <button className="file-tile" name={props.name} onClick={props.onClick}>{props.name}</button>
-            <button className="file-tile__remove-button" name={props.name} onClick={props.onRemove}>x</button>
+            <button className="file-tile__remove-button" name={props.name} onClick={props.onRemove}></button>
         </div>
     );
 }
