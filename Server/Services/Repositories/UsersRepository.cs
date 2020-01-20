400d4515e83cb56c4d7643f3e8e6beca02e73e92
SzymonWelter <szymon.welter98@gmail.com>
2020-01-20 03:31:29 +0100

    creating new user fixed

--- Server/Services/Repositories/UsersRepository.cs
+++ Server/Services/Repositories/UsersRepository.cs
@@ -8,18 +8,17 @@ using Server.Models.DAL;
 using Server.Models.Domain;
 using Server.Models.DTO;
 
-namespace Server.Services.Repositories
-{
-    internal class UsersRepository : IUsersRepository
-    {
+namespace Server.Services.Repositories {
+    internal class UsersRepository : IUsersRepository {
         private readonly WebAppContext _context;
 
-        public UsersRepository(WebAppContext webAppContext)
-        {
+        public UsersRepository(WebAppContext webAppContext) {
             _context = webAppContext;
         }
-        public async Task AddAsync(UserDAL user, FileDAL photo)
-        {
+        public async Task AddAsync(UserDAL user, FileDAL photo) {
+
+            photo.PublisherId = null;
+
             await _context.Files.AddAsync(photo);
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
@@ -30,33 +29,26 @@ namespace Server.Services.Repositories
             await _context.SaveChangesAsync();
         }
 
-        public async Task<bool> ExistsAsync(Func<UserDAL, bool> condition)
-        {
-            return await Task.Run(() =>
-            {
+        public async Task<bool> ExistsAsync(Func<UserDAL, bool> condition) {
+            return await Task.Run(() => {
                 var users = _context.Users.Where(condition);
                 return users.Count() != 0;
             });
         }
 
-        public async Task<string> GetUserId(string username)
-        {
-            return await Task.Run(() =>
-            {
-                var userId = _context.Users.Where( user => user.Login == username).First().UserId;
+        public async Task<string> GetUserId(string username) {
+            return await Task.Run(() => {
+                var userId = _context.Users.Where(user => user.Login == username).First().UserId;
                 return userId.ToString();
             });
         }
 
-        public async Task<bool> ExistsLoginAsync(string login)
-        {
+        public async Task<bool> ExistsLoginAsync(string login) {
             return await ExistsAsync(user => user.Login == login);
         }
 
-        public async Task<bool> PasswordIsValid(string username, string password)
-        {
-            return await Task.Run(() =>
-            {
+        public async Task<bool> PasswordIsValid(string username, string password) {
+            return await Task.Run(() => {
                 var user = _context.Users.Where(user => user.Login == username && user.Password == password);
                 var result = user.Count() != 0;
                 return result;
